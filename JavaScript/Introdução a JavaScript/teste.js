137aa4dd15a29e0735c5961a6d3fc8418c2440ba
VictorHugoSDev <victorhhugo@hotmail.com>
2022-06-11 18:39:27 -0300

    primeiro commit de JS, bootcamp DIO

---
+++ JavaScript/Introdução a JavaScript/teste.js
@@ -0,0 +1,14 @@
+//var y = 0; // serve para comentar uma linha
+
+/* var y = 0;
+var x = 1; */ /* serve para comentar varias linhas */
+
+// declarar variavel no JS, pode ser usado const (valores que não pode ser manipulados) ou pode ser usado var (valores que podem ser manipulados)
+
+// sintaxe de uma função
+function soma(a, b){
+    return a + b; // 8
+}
+
+// chamar a função
+soma(3, 5);
